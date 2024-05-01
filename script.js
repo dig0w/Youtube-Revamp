b03c04a86388230b2fedd6878795061992c422e1
dig0w <67347822+dig0w@users.noreply.github.com>
2024-05-01 15:22:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 // OnLoad
 window.onload = dislikeLoader();
+//OnClick
+window.onclick = () => {
+	console.log("click");
+	dislikeLoader();
+};
 
 // OnReload
 var oldHref = document.location.href;
 window.onload = () => {
     var bodyList = document.querySelector("body");
 
-    var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
+    var observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
             if (oldHref != document.location.href) {
                 oldHref = document.location.href;
 
@@ -16,25 +21,24 @@ window.onload = () => {
         });
     });
 
-    var config = {
+    observer.observe(bodyList, {
         childList: true,
         subtree: true
-    };
-    observer.observe(bodyList, config);
+    });
 };
 
 // Dislikes
 async function dislikeLoader() {
 	const url = window.location.href;
 
-	if(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/.test(url)){
+	if (/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/.test(url)) {
 		var videoId = url.match(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/)[7];
 			if(videoId.length != 11){ videoId = false };
 
 		const info = await VideoInfo(videoId);
 
 		upateDislikes(info.dislikes, 0, url);
-	} else if(/^.*((youtu.be\/)|(shorts\/))([^#&?]*).*/.test(url)){
+	} else if (/^.*((youtu.be\/)|(shorts\/))([^#&?]*).*/.test(url)) {
 		var videoId = url.match(/^.*((youtu.be\/)|(shorts\/))([^#&?]*).*/)[4];
 			if(videoId.length != 11){ videoId = false };
 
@@ -55,21 +59,21 @@ function upateDislikes(dislikesInfo, idx, url) {
 		var dislikes = 0;
 		var num = 0;
 	
-		if(dislikesInfo >= 1000000000){
+		if (dislikesInfo >= 1000000000) {
 			num = dislikesInfo/1000000000;
 			dislikes = `${Math.round(num*10)/10}B`;
-		} else if(dislikesInfo >= 1000000){
+		} else if (dislikesInfo >= 1000000) {
 			num = dislikesInfo/1000000;
 			dislikes = `${Math.round(num*10)/10}M`;
-		} else if(dislikesInfo >= 1000){
+		} else if (dislikesInfo >= 1000) {
 			num = dislikesInfo/1000;
 			dislikes = `${Math.round(num*10)/10}K`;
-		} else{
+		} else {
 			dislikes = dislikesInfo;
 		};
 	
-		try{
-			if(idx == 0){
+		try {
+			if (idx == 0) {
 				var dislikeBtn = document.querySelectorAll("dislike-button-view-model")[0].children[0].children[0].children[0];
 	
 				if (dislikeBtn.children[1].classList != "yt-spec-button-shape-next__button-text-content") {
@@ -85,19 +89,17 @@ function upateDislikes(dislikesInfo, idx, url) {
 				} else if (dislikeBtn.children[1].classList == "yt-spec-button-shape-next__button-text-content" && dislikeBtn.children[1].innerHTML != dislikes) {
 					dislikeBtn.children[1].innerHTML = dislikes;
 				};
-			} else if(idx == 1){
-				var dislikebtn = document.querySelectorAll("ytd-toggle-button-renderer#dislike-button.style-scope.ytd-like-button-renderer");
-	
-				dislikebtn[0].children[0];
+			} else if (idx == 1) {
+				var dislikeBtns = document.querySelectorAll("ytd-toggle-button-renderer#dislike-button.style-scope.ytd-like-button-renderer");
 			
-				for(var i = 0; i < dislikebtn.length; i++){
-					dislikebtn[i].children[0].children[0].children[1].children[0].innerHTML = dislikes;	
+				for (var i = 0; i < dislikeBtns.length; i++) {
+					if (dislikeBtns[i].children[0] && dislikeBtns[i].children[0].children[0] && dislikeBtns[i].children[0].children[0].children[1] && dislikeBtns[i].children[0].children[0].children[1].children[0]) {
+						dislikeBtns[i].children[0].children[0].children[1].children[0].innerHTML = dislikes;
+					};
 				};
 			};
-		} catch(err){
-			setTimeout(function() { upateDislikes(dislikesInfo, idx, url) }, 100);
+		} catch (err) {
+			setTimeout(() => { upateDislikes(dislikesInfo, idx, url) }, 100);
 		};
-	
-		setTimeout(function() { upateDislikes(dislikesInfo, idx, url) }, 100);
 	};
 };
